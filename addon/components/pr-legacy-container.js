b81cef5ca69fc2141329bc1a123a30da839e9b70
mihroot <mihrootk@gmail.com>
2016-10-04 17:37:08 +0300

    fixes

--- addon/components/pr-legacy-container.js
+++ addon/components/pr-legacy-container.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  modalClassName: '',
   nonMiddled: false,
   closable: true,
 
@@ -22,5 +23,17 @@ export default Ember.Component.extend({
         this.sendAction('close', _doTransition);
       }
     );
+  },
+
+  /**
+   * [willDestroyElement description]
+   * @return {[type]} [description]
+   */
+  willDestroyElement() {
+    this._super(...arguments);
+    // если модал открывается НЕ по отдельному урлу.(для остальных случаев есть mixin)
+    // помогает избежать активного диммера
+    // при работе юзера с историей браузера(при нажатии на кнопку назад)
+    window.Window.close(this.get('modalClassName'));
   }
 });
